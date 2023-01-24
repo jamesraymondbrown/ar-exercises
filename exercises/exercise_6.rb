d8d44ef4eb008eb68ef679866aa28867bb412624
James Brown <jamesraymondbrown@gmail.com>
2023-01-24 01:58:35 +0000

    add answers for exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,29 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+# We haven't used the Employee class (and employees table) at all yet. If you look at this table's column structure, you'll find that it has a `store_id` (integer) column. This is a column that identifies which store each employee belongs to. It points to the `id` (integer) column of their store.
+
+# Let's tell Active Record that these two tables are in fact related via the `store_id` column.
+
+# 1. Add the following code _directly_ inside the Store model (class): `has_many :employees`
+# 2. Add the following code directly inside the Employee model (class): `belongs_to :store`
+# 3. Add some data into employees. Here's an example of one (note how it differs from how you create stores): `@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)`
+# 4. Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the `@store#` instance variables that you defined in previous exercises. Create a bunch under `@store1` (Burnaby) and `@store2` (Richmond). Eg: `@store1.employees.create(...)`.
+
+class Store
+  has_many :employees
+end
+
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Joan", last_name: "Rivers", hourly_rate: 30)
+@store1.employees.create(first_name: "Gunther", last_name: "McGinn", hourly_rate: 50)
+@store2.employees.create(first_name: "James", last_name: "Brown", hourly_rate: 100)
+@store2.employees.create(first_name: "Joey", last_name: "Malone-Bahama", hourly_rate: 10)
+@store2.employees.create(first_name: "Ellen", last_name: "Stone", hourly_rate: 99)
+
+# bundle exec ruby exercises/exercise_6.rb
+
